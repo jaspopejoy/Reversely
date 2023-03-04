46bd4130d502b957598c3ca5a1e44e75b6cd8285
jaspopejoy <120056960+jaspopejoy@users.noreply.github.com>
2023-03-04 16:58:18 -0600

    removed extra images

--- js/site.js
+++ js/site.js
@@ -1,4 +1,39 @@
-// Call Hello World
-function helloworld{
-    alert("hello world");
+
+// Get the string from the page
+//Conttoller function
+function getValue() {
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementsByClassName("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+//Reverse the string
+//logic function
+function reverseString(userString) {
+
+    let revString = [];
+
+    //reverse a string using a for loop
+    for (let index = userString.length -1; index >= 0; index--) {
+        revString += userString[index];        
+    }
+
+    return revString;
+
+}
+
+
+//Display the string to the user
+//View function
+function displayString(revString) {
+
+    //write to the page
+    document.getElementById("msg").innerHTML = `Your String Reversed Is: ${revString}`;
+    //show the alert box
+
 }
