ead8f57fd5236f745d42c63d175f1252b01e8604
jaspopejoy <120056960+jaspopejoy@users.noreply.github.com>
2023-03-06 19:14:23 -0600

    added twitter link

--- js/site.js
+++ js/site.js
@@ -5,7 +5,7 @@ function getValues() {
 
     document.getElementById("alert").classList.add("invisible");
 
-    let userString = document.getElementsByClassName("userString").value;
+    let userString = document.getElementById("userString").value;
 
     let revString = reverseString(userString);
 
@@ -16,14 +16,14 @@ function getValues() {
 //logic function
 function reverseString(userString) {
 
-    let revString = [];    
+    let revString = [];
 
     //reverse a string using a for loop
     //decrementing for loop
-    for (let index = userString.length -1; index >= 0; index--) {
-        revString += userString[index];        
-    }       
-    
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+
     return revString;
 
 }
